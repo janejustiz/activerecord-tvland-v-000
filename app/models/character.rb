ffc7012ea5a35fc0ffcf0753c169f1e2c5fe0847
Jane Justiz <janejustiz@gmail.com>
2019-08-22 16:36:10 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
-  belongs_to :characters
-  
+  belongs_to :character
+
   def say_that_thing_you_say
     array = []
     array << self.name
