0008d2e78c57af0fca50b4100bb74c3cfd50ab2d
Jane Justiz <janejustiz@gmail.com>
2019-08-22 16:40:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :character
+  def actors_list
+    array = []
+    self.characters.actors.each do |actor|
+      array << actor.full_name
+    end
+    array
+  end
 end
