c83db6a4460a0a4a106c7fa32d4254efb249c2fb
Jane Justiz <janejustiz@gmail.com>
2019-08-22 16:27:51 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,12 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  def full_name
+    array = []
+    array << self.first_name
+    array << self.last_name
+    name = array.join(" ")
+    name
+  end
 end
