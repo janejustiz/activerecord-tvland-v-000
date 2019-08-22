6e39adf0253d3dbe08a37a830f8596d39abe4aac
Jane Justiz <janejustiz@gmail.com>
2019-08-22 16:51:24 +0000

    Automatically backed up by Learn

---
+++ db/migrate/007_add_day_genre_and_season_column_to_show.rb
@@ -0,0 +1,7 @@
+class AddDayGenreAndSeasonColumnToShow < ActiveRecord::Migrations[5.1]
+  def change
+    add_column :show, :day, :string
+    add_column :show, :genre, :string
+    add_column :show, :season, :string
+  end
+end
