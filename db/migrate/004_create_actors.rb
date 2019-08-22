4729bd36d96227992424efd12d324da9f5166bda
Jane Justiz <janejustiz@gmail.com>
2019-08-22 16:15:51 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,8 @@
 class CreateActors < AtiveRecord::Migrate[5.2]
-  create_table :actor |t|
-    t.string :first_name
-    t.string :last_name
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
